3efbea36756817b5d14f639eb4b4d7235f9331b8
Anuoluwapo Oyetibo <Aoyetibo@gmail.com>
2020-05-17 19:43:57 -0400

    Added a script to work the calculating functions. 	New File. Five seperate functions.

---
+++ script.js
@@ -0,0 +1,35 @@
+
+
+// VARIABLES
+var prevAns;
+function insert(num){
+    // document.form.textview.value=document.form.textview.value+num;
+    document.form.textview.value+=num;
+    
+}
+function equal(){
+    var exp=document.form.textview.value;
+    if(exp){
+        document.form.textview.value=eval(exp);
+        prevAns = eval(exp);
+    }
+    document.getElementById("res").innerHTML = "Ans: "+prevAns;
+
+}
+function clear(){
+   document.form.textview.value= ' ';
+
+}
+function testClear(){
+    clear();
+}
+function back(){
+   var exp=document.form.textview.value;
+   document.form.textview.value=exp.substring(0,exp.length-1);
+}
+function getAns(){
+    
+    // document.form.textview.value = prevAns;
+    document.getElementById("res").innerHTML = "PrevAns: "+prevAns;
+}
+
